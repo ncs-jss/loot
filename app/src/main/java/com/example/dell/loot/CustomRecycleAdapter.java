e05985e1427e3983625e7a890692d2fcbfb30f32
Shobhit Agarwal <shobhit.shobhitagarwal.agarwal@gmail.com>
2018-03-09 15:04:37 +0530

    online_users

---
+++ app/src/main/java/com/example/dell/loot/CustomRecycleAdapter.java
@@ -0,0 +1,8 @@
+package com.example.dell.loot;
+
+/**
+ * Created by shobhit on 3/9/2018.
+ */
+
+public class CustomRecycleAdapter {
+}
---
+++ app/src/main/java/com/example/dell/loot/OnlineUsers.java
@@ -0,0 +1,29 @@
+package com.example.dell.loot;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class OnlineUsers extends Fragment {
+
+
+    public OnlineUsers() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_online_users, container, false);
+    }
+
+}
